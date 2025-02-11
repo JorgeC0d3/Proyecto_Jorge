4760af70f4e1813c60b709bc10e65dadc4ee76e7
Jorge Pérez <158076905+JorgeC0d3@users.noreply.github.com>
2025-02-11 17:25:14 +0100

    Update dashboard.component.ts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { urlBackend } from '../config';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  providers: [DatePipe],
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  tasks: any[] = [];
+
+  constructor(private datePipe: DatePipe) {}
+
+  formatearFecha(fecha: any): string | null {
+    return this.datePipe.transform(fecha, 'dd/MM/yyyy HH:mm');
+  }
+
+  getTasks(){
+    fetch(`${urlBackend}/api/alltasks`)
+    .then(response => response.json())
+    .then(tasks => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+    })
+  }
+
+  ngOnInit(){
+    this.getTasks();
+  }
 
 }
