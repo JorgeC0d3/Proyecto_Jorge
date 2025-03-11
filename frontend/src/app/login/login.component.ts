3162f0bb25e1c107177f561ce37ed7f5dceff7d1
Jorge Pérez <158076905+JorgeC0d3@users.noreply.github.com>
2025-03-11 17:33:02 +0100

    Add files via upload

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import {urlBackend} from '../config';
 import {Router, RouterLink} from '@angular/router';
 
+declare var bootstrap: any; //Variable global para utilizar bootstrap en el componente
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -14,5 +16,78 @@ export class LoginComponent {
 
   email: string = '';
   password: string = '';
+
+  async signin(){
+  
+      if(this.email === '' || this.password === ''){
+        this.warningToast();
+        return;
+      }
+  
+      const userData = {
+        email: this.email,
+        password: this.password
+      }
+  
+      try{
+  
+        const response = await fetch(`${urlBackend}/api/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userData)
+        })
+  
+        const data = await response.json();
+
+        if(data.message === 'Usuario no encontrado'){
+          this.infoToast();
+          return;
+        }
+
+        if(data.message === 'Contraseña incorrecta'){
+          this.warningToast();
+          return;
+        }
+
+        console.log(data);
+        this.email = '';
+        this.password = '';
+  
+        this.successToast();
+  
+      }catch(error){
+        this.errorToast();
+      }
+    }
+  
+    //Toast Error
+    errorToast(){
+      const toastElement = document.getElementById("toast-error");
+      const toast = new bootstrap.Toast(toastElement!);
+      toast.show();
+    }
+  
+    //Toast Success
+    successToast(){
+      const toastElement = document.getElementById("toast-success");
+      const toast = new bootstrap.Toast(toastElement!);
+      toast.show();
+    }
+  
+    //Toast Warning
+    warningToast(){
+      const toastElement = document.getElementById("toast-warning");
+      const toast = new bootstrap.Toast(toastElement!);
+      toast.show();
+    }
+  
+    //Toast Info
+    infoToast(){
+      const toastElement = document.getElementById("toast-info");
+      const toast = new bootstrap.Toast(toastElement!);
+      toast.show();
+    }
 
 }
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import {urlBackend} from '../config';
 import {Router, RouterLink} from '@angular/router';
 
+declare var bootstrap: any; //Variable global para utilizar bootstrap en el componente
+
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -15,5 +17,78 @@ export class SignupComponent {
   surnames: string = '';
   email: string = '';
   password: string = '';
+
+  async signup(){
+
+    if(this.name === '' || this.surnames === '' || this.email === '' || this.password === ''){
+      this.warningToast();
+      return;
+    }
+
+    const userData = {
+      name: this.name,
+      surnames: this.surnames,
+      email: this.email,
+      password: this.password
+    }
+
+    try{
+
+      const response = await fetch(`${urlBackend}/api/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+
+      const data = await response.json();
+      console.log(data);
+      this.name = '';
+      this.surnames = '';
+      this.email = '';
+      this.password = '';
+
+      if(data.message === 'El usuario ya existe'){
+        this.infoToast();
+        return;
+      }
+
+      this.successToast();
+
+    }catch(error){
+      this.errorToast();
+      console.log('Error al dar de alta el usuario');
+    }
+  }
+
+  //Toast Error
+  errorToast(){
+    const toastElement = document.getElementById("toast-error");
+    const toast = new bootstrap.Toast(toastElement!);
+    toast.show();
+  }
+
+  //Toast Success
+  successToast(){
+    const toastElement = document.getElementById("toast-success");
+    const toast = new bootstrap.Toast(toastElement!);
+    toast.show();
+  }
+
+  //Toast Warning
+  warningToast(){
+    const toastElement = document.getElementById("toast-warning");
+    const toast = new bootstrap.Toast(toastElement!);
+    toast.show();
+  }
+
+  //Toast Info
+  infoToast(){
+    const toastElement = document.getElementById("toast-info");
+    const toast = new bootstrap.Toast(toastElement!);
+    toast.show();
+  }
+
 
 }
